2ed911b37bd531610e18135ad2a0de0aa0d116f5
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-02-28 13:14:36 +0200

    Create nodejs.js
    node js

---
+++ nodejs.js
@@ -0,0 +1,19 @@
+let isLightOn = false;
+
+function turnOn() {
+    isLightOn = true;
+    console.log("Light turned on");
+    // Add code to control the light switch and turn the light on
+}
+
+function turnOff() {
+    isLightOn = false;
+    console.log("Light turned off");
+    // Add code to control the light switch and turn the light off
+}
+
+// Example: Turn the light on
+turnOn();
+
+// Example: Turn the light off
+turnOff();
